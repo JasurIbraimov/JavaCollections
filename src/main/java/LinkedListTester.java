43f3c84f6f2e32e6997de508c582a7c328fa494b
Jasur <ibraimov0409@mail.ru>
2022-12-19 19:32:15 +0600

    Working with Iterators

--- src/main/java/LinkedListTester.java
+++ src/main/java/LinkedListTester.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class LinkedListTester {
@@ -6,5 +7,27 @@ public class LinkedListTester {
         linkedList.add(10);
         System.out.println(linkedList.size());
         System.out.println(linkedList);
+
+        // Iterators
+        LinkedList<String> guitars = new LinkedList<>();
+        guitars.add("Gibson");
+        guitars.add("Jackson");
+        guitars.add("Taylor");
+        guitars.add("Fender");
+
+        // Create iterator
+        Iterator<String> iterator = guitars.iterator();
+        while (iterator.hasNext()) {
+            String guitar = iterator.next();
+            if (guitar.equals("Taylor")) {
+                iterator.remove();
+            }
+        }
+        // Same as foreach loop
+        for(String guitar : guitars) {
+            System.out.println(guitar);
+        }
+
+
     }
 }
